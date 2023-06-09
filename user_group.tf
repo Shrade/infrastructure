7d52145982aa6bceaba3a306f391d3e2cfccfe6c
Shrade <cheok1997@gmail.com>
2023-06-10 00:44:07 +0800

    add policy

--- user_group.tf
+++ user_group.tf
@@ -2,3 +2,20 @@ resource "aws_iam_group" "example" {
   name = "admin-shrade"
   path = "/admin/"
 }
+
+resource "aws_iam_policy" "example" {
+  name        = "my-user-group-policy"
+  description = "Example IAM policy for my user group"
+  policy      = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": "*",
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
