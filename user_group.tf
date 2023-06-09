7c3a879481d037fb5981741d0a7aafdc14a98542
Shrade <cheok1997@gmail.com>
2023-06-10 00:59:28 +0800

    fix identation

--- user_group.tf
+++ user_group.tf
@@ -3,19 +3,19 @@ resource "aws_iam_group" "that" {
   path = "/admin/"
 }
 
-resource "aws_iam_policy" "that" {
-  name        = "user_policy"
+resource "aws_iam_policy" "example" {
+  name        = "my-user-group-policy"
   description = "Example IAM policy for my user group"
   policy      = <<EOF
 {
-"Version": "2012-10-17",
-"Statement": [
+  "Version": "2012-10-17",
+  "Statement": [
     {
-    "Effect": "Allow",
-    "Action": "*",
-    "Resource": "*"
+      "Effect": "Allow",
+      "Action": "*",
+      "Resource": "*"
     }
-]
+  ]
 }
 EOF
 }
