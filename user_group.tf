c45439256d6dbd20ebb180c47ccdaa550760c9a6
Shrade <cheok1997@gmail.com>
2023-06-10 00:57:09 +0800

    test

--- user_group.tf
+++ user_group.tf
@@ -1,26 +1,35 @@
-resource "aws_iam_group" "example" {
+resource "aws_iam_group" "that" {
   name = "admin-shrade"
   path = "/admin/"
 }
 
-resource "aws_iam_policy" "example" {
-  name        = "my-user-group-policy"
+resource "aws_iam_policy" "that" {
+  name        = "user_policy"
   description = "Example IAM policy for my user group"
   policy      = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Action": "*",
-      "Resource": "*"
-    }
-  ]
-}
-EOF
+                {
+                "Version": "2012-10-17",
+                "Statement": [
+                    {
+                    "Effect": "Allow",
+                    "Action": "*",
+                    "Resource": "*"
+                    }
+                ]
+                }
+                EOF
 }
 
-resource "aws_iam_group_policy_attachment" "example" {
-  group      = aws_iam_group.example.name
-  policy_arn = aws_iam_policy.example.arn
+resource "aws_iam_user" "shrade_general" {
+  name = "shrade-general"
+}
+
+resource "aws_iam_group_policy_attachment" "that" {
+  group      = aws_iam_group.that.name
+  policy_arn = aws_iam_policy.that.arn
+}
+
+resource "aws_iam_user_policy_attachment" "that" {
+  user      = aws_iam_user.shrade_general.name
+  policy_arn = aws_iam_policy.that.arn
 }
