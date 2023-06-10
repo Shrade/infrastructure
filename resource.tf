de4336dcaaff064bc15e09edc1d36158c3a01cfc
Shrade <cheok1997@gmail.com>
2023-06-10 21:32:54 +0800

    hard code subnet for now

--- resource.tf
+++ resource.tf
@@ -129,7 +129,7 @@ resource "aws_instance" "shrade_ec2" {
   instance_type = "t2.micro" 
 
   vpc_security_group_ids = [aws_security_group.this.id]
-  subnet_id = aws_subnet.private[count.index].id
+  subnet_id = "subnet-0b6155ee14a343d8f"
   tags = {
     Name = "shrade_ec2"
   }
